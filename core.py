6adc2b2bcdca865798992de1cc9b78c56c786270
CtkGHoSt <ctk915798130@gmail.com>
2018-12-27 19:03:37 +0800

    重构 write config 代码

--- core.py
+++ core.py
@@ -47,22 +47,9 @@ class mainWin(Frame.MyFrame):
 
 # 写入配置文件
 def writeConfig(self):
-    date = "[user]\n" \
-           "userid =" + self.userid + "\n" \
-            "password =" + self.password + "\n" \
-            "check ="+ str(self.check) +"\n\n"\
-           "[run]\n" \
-           "time_unit = minutes\n" \
-           "every_time = 5\n" \
-           "begin_time = 07:00\n" \
-           "end_time = 23:59\n" \
-            "log_level = debug"
-    # if os.path.exists(config_file):
-    #     os.remove(config_file)
-    f = open(self.config_file, 'w')
-    f.write(date)
-    f.close()
-    # hideFile(config_file)
+    self.conf.set('user', 'userid', self.userid)
+    self.conf.set('user', 'password', self.password)
+    self.conf.set('user', 'check', str(self.check))
 
 def readConfig(self):
     self.conf.read(self.config_file, encoding='utf-8')
