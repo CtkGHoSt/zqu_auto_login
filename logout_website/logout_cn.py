feec66670c605de6c6ebc480cf513d67736e2771
CtkGHoSt <ctk915798130@gmail.com>
2019-03-10 00:59:38 +0800

    远程下载 web端

--- logout_website/logout_cn.py
+++ logout_website/logout_cn.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, abort
 
 app = Flask('logout')
 
@@ -14,6 +14,17 @@ def test_form_func():
 def main():
     return render_template('logout.html')
 
+@app.route('/logout', methods=['GET', 'POST'])
+def logout_api():
+    userid = request.values.get('userid')
+    token = request.values.get('token')
+    if request.method == 'GET':
+        if userid == '201624131440' and token == 'test123':
+            return ''
+        else:
+            abort(404)
+    elif request.method == 'POST':
+        return '<h1>REMOTE LOGOUT SUCCESS!</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5001,debug=False)
