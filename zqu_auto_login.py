5a91e4b1a4e2d38357f7995bd9125e5d5e0ce263
chiihero <853879993@qq.com>
2019-03-09 19:55:57 +0800

    update

--- zqu_auto_login.py
+++ zqu_auto_login.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import datetime
 import re
-import wx  # 引入wx模块
+import wx
 import frame
 import login
 import schedule
@@ -9,17 +9,16 @@ import os
 from time import sleep
 import platform
 import threading
-from configparser import ConfigParser
 import logging
 from logging.handlers import TimedRotatingFileHandler
 import sys
 
-
 from config import file_abspath, location, logger, conf
+
 
 # 创建mainWin类并传入my_win.MyFrame
 class MainWin(frame.MyFrame):
-    def GetValue(self, event):
+    def getvalue(self, event):
         userid = self.userid.GetValue()
         password = self.password.GetValue()
         check = self.check_start.GetValue()
@@ -28,7 +27,7 @@ class MainWin(frame.MyFrame):
     def open(self, event):
         if self.btn_open.GetLabel() == "开启":
             self.btn_open.SetLabel("关闭")
-            userid, password, check = main_win.GetValue(self)
+            userid, password, check = main_win.getvalue(self)
             conf.set('user', 'userid', value=userid)
             conf.set('user', 'password', password)
             conf.set('user', 'check', str(check))
@@ -49,11 +48,11 @@ class MainThread(threading.Thread):
         self.auto_start()
         logger.info('学号：' + self.userid + ' 密码:' + self.password)
         self.login_start()
-    
+
     def auto_start(self):
-        '''
+        """
         开机启动
-        '''
+        """
         #######删除历史使用#######
         cmd = 'reg delete "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run" /v AutoLogin /f '
         os.system(cmd)
@@ -69,9 +68,9 @@ class MainThread(threading.Thread):
                     os.system(cmd)
             except:
                 logger.error('修改开机项失败')
-    
+
     def login_start(self):
-        '''子线程要执行的代码'''
+        """子线程要执行的代码"""
         logger.debug("第一次运行测试")
         login.main(self.userid, self.password)  # 第一次启动
         sleep(5)
@@ -89,6 +88,7 @@ class MainThread(threading.Thread):
             schedule.run_pending()
             sleep(1)
 
+
 def init_log():
     level = conf.get('run', 'log_level')
     if level == 'info':
@@ -102,16 +102,16 @@ def init_log():
     """
     run_log = location + "\\log\\run.log"
     logger.setLevel(log_level)
-    format =logging.Formatter("[%(asctime)s]-%(levelname)-6s %(module)s:%(lineno)d - %(message)s","%m-%d %H:%M:%S")
+    format = logging.Formatter("[%(asctime)s]-%(levelname)-6s %(module)s:%(lineno)d - %(message)s", "%m-%d %H:%M:%S")
     """
     切割日志
     结果是每1天生成一个日志文件，保留最近10次的日志文件,MIDNIGHT
     when参数可以设置S M H D,分别是秒、分、小时、天分割，也可以按周几分割，也可以凌晨分割
     """
     handler = TimedRotatingFileHandler(run_log, when='MIDNIGHT', interval=1,
-                                                             backupCount=30,encoding='utf-8',
-                                                             atTime=datetime.time(0, 0, 0, 0))
-    handler.suffix = "%Y-%m-%d.log"#切割后的日志设置后缀
+                                       backupCount=30, encoding='utf-8',
+                                       atTime=datetime.time(0, 0, 0, 0))
+    handler.suffix = "%Y-%m-%d.log"  # 切割后的日志设置后缀
     handler.extMatch = re.compile(r"^\d{4}-\d{2}-\d{2}.log$")
     handler.setLevel(log_level)
     handler.setFormatter(format)
@@ -122,13 +122,13 @@ def init_log():
     console.setFormatter(format)
     logging.getLogger().addHandler(console)
 
-
+"""
 # 隐藏配置文件
 def hideFile(file_abspath):
     if 'Windows' in platform.system():
         cmd = 'attrib +h "' + file_abspath + '"'
         os.system(cmd)
-
+"""
 
 if __name__ == '__main__':
     # 初始化程序
